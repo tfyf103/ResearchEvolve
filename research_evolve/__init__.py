c36f6da0ee2b32e372bd2b8e9c85e71cad908d3f
tfyf103 <107088126+tfyf103@users.noreply.github.com>
2026-08-11 18:25:51 +0800

    export v0.3 semantic research API

--- research_evolve/__init__.py
+++ research_evolve/__init__.py
@@ -1,6 +1,17 @@
 """ResearchEvolve public API."""
 
-from .spec import Budget, Constraint, Objective, ResearchSpec, SearchPolicy
+from .ideas import IdeaGenome, ResearchProposal, SemanticPatch
+from .spec import Budget, Constraint, ExplorerPolicy, Objective, ResearchSpec, SearchPolicy
 
-__all__ = ["ResearchSpec", "Objective", "Constraint", "Budget", "SearchPolicy"]
-__version__ = "0.2.0"
+__all__ = [
+    "ResearchSpec",
+    "Objective",
+    "Constraint",
+    "Budget",
+    "SearchPolicy",
+    "ExplorerPolicy",
+    "IdeaGenome",
+    "ResearchProposal",
+    "SemanticPatch",
+]
+__version__ = "0.3.0"
